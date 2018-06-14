9d5be407bc6adb726e314b22179caf4d14b51579
Joel McCoy <joelmccoy+github@gmail.com>
2018-06-14 14:37:32 -0400

    maintenance

--- index.js
+++ index.js
@@ -8,18 +8,16 @@ var promise = require('bluebird');
 
 var T = new Twit(credentials);
 
-// Search for tweets containing "I love when" or "I hate when"
-
-// Take array of phrases, or them together, search and send to cb
+// Take array of phrases, or them together, return a Promise
 function phraseSearch(phrases) {
   phrases = phrases.join(" OR ");
-  params = {q: phrases, count: 50};
+  params = {q: phrases, count: 100};
   return T.get('search/tweets', params);
 }
 
 function tweetFilter(tweet) {
   // console.log("In tweetFilter looking at:", tweet.text);
-  var noGood = wordfilter.blacklisted(tweet.text) || tweet.text.indexOf('@') > -1 || tweet.text.length > 116;
+  var noGood = wordfilter.blacklisted(tweet.text) || tweet.text.indexOf('@') > -1 || tweet.text.length > 150;
   return !noGood
 }
 
@@ -29,9 +27,9 @@ function cleanupCandidates(tweets) {
   return cleanTweets;
 }
 
-function pickAndSubstitute(data) {
-  console.log("Working with", data.statuses.length, "tweets pre-filter");
-  var cleanTweets = cleanupCandidates(data.statuses);
+function pickAndSubstitute(statuses) {
+  console.log("Working with", statuses.length, "tweets pre-filter");
+  var cleanTweets = cleanupCandidates(statuses);
   console.log("Working with", cleanTweets.length, "tweets post-filter");
   var statusToUse = _.sample(cleanTweets);
 
@@ -43,33 +41,37 @@ function pickAndSubstitute(data) {
   return textToUse;
 }
 
-function getFaceUrl(data) {
-    var faceTweet = _.sample(data.statuses);
+function getFaceUrl(statuses) {
+    var faceTweet = _.sample(statuses);
     var faceUrl = faceTweet.entities.media[0].url;
     console.log("using face img:", faceUrl);
     return faceUrl;
 }
 
 function searchFace() {
-  return T.get('search/tweets', {q: "from:TVFaceBot", count: 75});
+  // return T.get('search/tweets', {q: "from:TVFaceBot", count: 75});
+  return T.get('statuses/user_timeline', {screen_name: "tvfacebot", trim_user:true, exclude_replies: true, count: 75});
 }
 
 function composeTweet(text, url) {
   return _.unescape(text) + " " + url;
 }
 
+// Search for tweets containing "I love when" or "I hate when"
+// get a picture of a face (probably)
+// tweet them out together
 var text = phraseSearch(['"i love when"', '"i hate when"']);
 var url = searchFace();
 
 promise.all([text, url]).then(function(responses) {
   // console.log("Working with responses:", responses);
-  var text = pickAndSubstitute(responses[0].data);
+  var text = pickAndSubstitute(responses[0].data.statuses);
   var url = getFaceUrl(responses[1].data);
   console.log("working with text/url:", text, url);
   var toTweet = composeTweet(text, url);
   T.post('statuses/update', {status: toTweet})
     .then(function(res) {
-      console.log("We twote!");
+      console.log("MFW twote!");
       console.log("\t", res.data.text);
     });
 });
